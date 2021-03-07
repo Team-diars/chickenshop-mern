b138d49e785c75d6046bf832a4f40f79aa13f486
Hector Herrera <hectorsum81@hotmail.com>
2021-03-07 12:14:36 -0500

    Porduct model added

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    require:true
+  },
+  price:{
+    type:Number,
+    require:true
+  },
+  state:{
+    type:Number,
+    default: 1
+  },
+})
+
+module.exports = Product = mongoose.model('product',ProductSchema);
