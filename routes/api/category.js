cbf1f9b6b71c1f4225d2926e56b71dcb807d00cd
Diego <csjoin99@gmail.com>
2021-03-07 18:02:21 -0500

    Comments and middleware modified

--- routes/api/category.js
+++ routes/api/category.js
@@ -10,22 +10,37 @@ const {
 } = require("../../controller/category");
 const { fieldValidation } = require("../../middleware/fieldValidation");
 
+//* @route  GET api/category
+//* @des    Test Route
+//* @access Private
 router.get("/", getCategories);
 
+//* @route  GET api/category/:id
+//* @des    Test Route
+//* @access Private
 router.get("/:id", getCategory);
 
+//* @route  POST api/category/register
+//* @des    Test Route
+//* @access Private
 router.post(
   "/register",
   [check("name", "Name is required").not().isEmpty(), fieldValidation],
   registerCategory
 );
 
+//* @route  PUT api/category/update/:id
+//* @des    Test Route
+//* @access Private
 router.put(
   "/update/:id",
   [check("name", "Name is required").not().isEmpty(), fieldValidation],
   updateCategory
 );
 
+//* @route  DELETE api/category/delete/:id
+//* @des    Test Route
+//* @access Private
 router.delete("/delete/:id", deleteCategory);
 
 module.exports = router;
--- routes/api/employee.js
+++ routes/api/employee.js
@@ -10,46 +10,69 @@ const {
 } = require("../../controller/employee");
 const { fieldValidation } = require("../../middleware/fieldValidation");
 
+//* @route  GET api/employee
+//* @des    Test Route
+//* @access Private
 router.get("/", getEmployees);
 
+//* @route  GET api/employee/:id
+//* @des    Test Route
+//* @access Private
 router.get("/:id", getEmployee);
 
+//* @route  POST api/employee/register
+//* @des    Test Route
+//* @access Private
 router.post(
   "/register",
   [
     check("name", "Name is required").not().isEmpty(),
     check("lastname", "Lastname is required").not().isEmpty(),
+    check("address", "Address is required").not().isEmpty(),
+  ],
+  [
     check("dni", "DNI is required").not().isEmpty(),
     check("dni", "DNI length must be 8 characters").isLength({
       min: 8,
       max: 8,
     }),
-    check("address", "Address is required").not().isEmpty(),
+  ],
+  [
     check("email", "Email is required").not().isEmpty(),
     check("email", "The format is incorrect").isEmail(),
-    fieldValidation,
   ],
+  fieldValidation,
   registerEmployee
 );
 
+//* @route  PUT api/employee/update/:id
+//* @des    Test Route
+//* @access Private
 router.put(
   "/update/:id",
   [
     check("name", "Name is required").not().isEmpty(),
     check("lastname", "Lastname is required").not().isEmpty(),
+    check("address", "Address is required").not().isEmpty(),
+  ],
+  [
     check("dni", "DNI is required").not().isEmpty(),
     check("dni", "DNI length must be 8 characters").isLength({
       min: 8,
       max: 8,
     }),
-    check("address", "Address is required").not().isEmpty(),
+  ],
+  [
     check("email", "Email is required").not().isEmpty(),
     check("email", "The format is incorrect").isEmail(),
-    fieldValidation,
   ],
+  fieldValidation,
   updateEmployee
 );
 
+//* @route  DELETE api/employee/delete/:id
+//* @des    Test Route
+//* @access Private
 router.delete("/delete/:id", deleteEmployee);
 
 module.exports = router;
