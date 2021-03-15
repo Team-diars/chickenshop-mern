dacfdfbafe74fda385294123cbaa4aa53f6e9fbb
Hector Herrera <hectorsum81@hotmail.com>
2021-03-15 10:36:05 -0500

    routes moved here

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import "./bootstrap.min.css";
-import { AppRouter } from "./routes/AppRouter";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import HomeScreen from "./components/landing/HomeScreen";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import LoginScreen from "./components/login/LoginScreen";
+import ProductScreen from "./components/products/ProductScreen";
+
 function App() {
-  return <AppRouter />;
+  return (
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen}/>
+          <Route exact path="/products" component={ProductScreen}/>
+          <Route exact path="/login" component={LoginScreen}/>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
