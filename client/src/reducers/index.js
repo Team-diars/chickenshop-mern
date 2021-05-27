d45bac403f2e567db4c08c5c5af3fbbf0e2545b1
Hector Herrera <hectorsum81@hotmail.com>
2021-05-26 22:33:46 -0500

    setting up my reducers into combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import {auth} from './auth';
 import {product} from './product';
 import {employee} from './employee';
 import {user} from './user';
+import {settings} from './settings';
 export default combineReducers({
   alert,
   auth,
   product,
   employee,
-  user
+  user,
+  settings
 });
