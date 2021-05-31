7a2192a75200f444547d5d69425348cca54a4ce9
Hector Herrera <hectorsum81@hotmail.com>
2021-05-31 01:20:53 -0500

    working on saveTicket method

--- client/src/components/orders/TableCart.js
+++ client/src/components/orders/TableCart.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Table } from 'react-bootstrap'
+import {addTicket} from '../../actions/ticket'
+import { connect } from 'react-redux';
+import { Button, Table } from 'react-bootstrap'
 
-const TableCart = ({cart}) => {
-  const {dish_name,dish_quantity} = cart;
+const TableCart = ({cart,num_table,addTicket}) => {
+  
+  const saveTicket = () => {
+    const data = cart.map(item => {
+      console.log("item > ",item)
+      // return {_id:item.dish_id, quantity:item.dish_quantity} || 
+      //       {_id:item.drink_id, quantity:item.drink_quantity} ||
+      //       {_id:item.salad_id, quantity:item.salad_quantity}
+    })
+    console.log(data);
+    // addTicket({
+    //   num_table,
+    // })
+  }
   return (
     <div>
       <Table bordered striped hover responsive size="sm">
@@ -17,19 +31,26 @@ const TableCart = ({cart}) => {
           {
             cart.map((dish,idx) => (
               <tr key={idx}>
-                <th>{dish.dish_name}</th>
-                <th>{dish.dish_quantity}</th>
+                <th>{dish.dish_name ?? dish.drink_name ?? dish.salad_name}</th>
+                <th>{dish.dish_quantity ?? dish.drink_quantity ?? dish.salad_quantity}</th>
               </tr>
             ))
           }
         </tbody>
       </Table>
+      <Button type="submit" className='btn-secondary add-button' onClick={saveTicket}>
+        Add Order <i class="fas fa-cart-plus"></i>
+      </Button>
     </div>
   )
 }
 
 TableCart.propTypes = {
-
+  addTicket: PropTypes.func.isRequired,
 }
 
-export default TableCart
+const mapStateToProps = state => ({
+  ticket: state.ticket,
+})
+
+export default connect(mapStateToProps,{addTicket})(TableCart)
