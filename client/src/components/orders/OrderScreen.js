31eabc5cbe1d0f0f2b7ba2da9a2cf4474addc1bd
Hector Herrera <hectorsum81@hotmail.com>
2021-05-31 01:20:13 -0500

    setting up getProducts method

--- client/src/components/orders/OrderScreen.js
+++ client/src/components/orders/OrderScreen.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Form, Col, Row, Table, Button } from 'react-bootstrap'
+import {getProducts} from '../../actions/product'
+import { connect } from 'react-redux';
 import TableCart from './TableCart'
 
-const OrderScreen = () => {
+const OrderScreen = ({getProducts, product:{products,loading}}) => {
+  console.log(products)
   const [cart, setCart] = useState([]);
+  const [num_table, setNumTable] = useState(0);
   const [formDataDishes, setFormDataDishes] = useState({
     dish_name:'',
-    dish_quantity:0
+    dish_quantity:0,
+    dish_id:null
+  });
+  const [formDataDrinks, setFormDataDrinks] = useState({
+    drink_name:'',
+    drink_quantity:0,
+    drink_id:null
   })
-  const {dish_name,dish_quantity} = formDataDishes;
-  const dishes = [
-    {
-      'idx':0,
-      'dish_name':'Arroz con pollo',
-      'dish_price':12.5
-    },
-    {
-      'idx':1,
-      'dish_name':'Pollo a la brasa 1/4',
-      'dish_price':23
-    }
-  ];
+  const [formDataSalads, setFormDataSalads] = useState({
+    salad_name:'',
+    salad_quantity:0,
+    salad_id:null
+  })
+  const {dish_id,dish_name,dish_quantity} = formDataDishes;
+  const {drink_id,drink_name,drink_quantity} = formDataDrinks;
+  const {salad_id, salad_name,salad_quantity} = formDataSalads;
+
+  const onChange = (e) => {
+    const { name, value, type } = e.target;
+    setNumTable({
+      ...num_table,
+      [name]: type === "number" ? parseInt(value) : value
+    });
+  }
+
   const onChangeDishes = (e) => {
     const { name, value, type } = e.target;
     setFormDataDishes({
@@ -29,30 +43,95 @@ const OrderScreen = () => {
     });
   }
   
+  const onChangeDrinks = (e) => {
+    const { name, value, type } = e.target;
+    setFormDataDrinks({
+      ...formDataDrinks,
+      [name]: type === "number" ? parseInt(value) : value
+    });
+  }
+
+  const onChangeSalads = (e) => {
+    const { name, value, type } = e.target;
+    setFormDataSalads({
+      ...formDataSalads,
+      [name]: type === "number" ? parseInt(value) : value
+    });
+  }
+
   const addDish = () => {
-    // if (cart.length > 0){
-    //   const index = cart.indexOf(item => item.dish_name = dish_name);
-    //   console.log(index);
-      
-    // }
-    
     const item = cart.find(item => item.dish_name === dish_name);
+    const product = products.find(item => (item.name === dish_name && item.status))
     if(item) {
       setFormDataDishes({
+        dish_id: product._id,
         dish_name:'',
         dish_quantity:0
       })
       return item.dish_quantity += dish_quantity;
     }
     setCart([...cart, {
+      dish_id: product._id,
       dish_name,
       dish_quantity: dish_quantity
     }])
     setFormDataDishes({
+      dish_id: null,
       dish_name:'',
       dish_quantity:0
     })
   }
+
+  const addDrink = () => {
+    const item = cart.find(item => item.drink_name === drink_name);
+    const product = products.find(item => (item.name === drink_name && item.status))
+    console.log(product._id);
+    if(item) {
+      setFormDataDishes({
+        drink_id: product._id,
+        drink_name:'',
+        drink_quantity:0
+      })
+      return item.drink_quantity += drink_quantity;
+    }
+    setCart([...cart, {
+      drink_id: product._id,
+      drink_name,
+      drink_quantity
+    }])
+    setFormDataDishes({
+      drink_id: null,
+      drink_name:'',
+      drink_quantity:0
+    })
+  }
+
+  const addSalad = () => {
+    const item = cart.find(item => item.salad_name === salad_name);
+    const product = products.find(item => (item.name === salad_name && item.status))
+    if(item) {
+      setFormDataSalads({
+        salad_id: product._id,
+        salad_name:'',
+        salad_quantity:0
+      })
+      return item.salad_quantity += salad_quantity;
+    }
+    setCart([...cart, {
+      salad_id: product._id,
+      salad_name,
+      salad_quantity
+    }])
+    setFormDataSalads({
+      salad_id: null,
+      salad_name:'',
+      salad_quantity:0
+    })
+  }
+
+  useEffect(()=>{
+    getProducts();
+  },[getProducts])
   return (
     <>
       <div className="m-0 col-lg-12">
@@ -60,6 +139,14 @@ const OrderScreen = () => {
       </div>
       <div className="m-0 p-0 row col-lg-12">
         <div className="col-md-6 form-wrapper">
+          <div className="p-0 col-12 col-md-5">
+            <label>Table number</label>
+            <Form.Control name="num_table" 
+                        type="number"
+                        value={num_table} 
+                        onChange={ e => onChange(e)}
+                        />
+          </div>
           <div className="mb-2">
             <label>Dishes</label>
             <div className="row m-0 p-0 col-12 col-sm-12">
@@ -72,8 +159,8 @@ const OrderScreen = () => {
                               >
                   <option value="">-- Select a dish --</option>
                   {
-                    dishes.map((dish) => (
-                      <option key={dish.idx} value={dish.dish_name}>{dish.dish_name}</option>
+                    products.map((product) => (
+                      (product.category === 'dishes') && <option key={product._id} value={product.name}>{product.name}</option>
                     ))
                   }
                 </Form.Control>
@@ -91,19 +178,30 @@ const OrderScreen = () => {
             </div>
           </div>
           <div className="mb-2">
-            <label>Extras</label>
+            <label>Drinks</label>
             <div className="row m-0 p-0 col-12 col-sm-12">
               <div className="p-0 input-wrapper col-10 col-sm-10">
                 <Form.Control as="select"
-                              name="category"
-                              custom 
+                              name="drink_name"
+                              value={drink_name}
+                              onChange={ e => onChangeDrinks(e) }
+                              custom
                               >
-                  <option value="">-- Select an extra --</option>
+                  <option value="">-- Select a drink --</option>
+                  {
+                    products.map((product) => (
+                      (product.category === 'drinks') && <option key={product._id} value={product.name}>{product.name}</option>
+                    ))
+                  }
                 </Form.Control>
-                <Form.Control name="name" type="number"/>
+                <Form.Control name="drink_quantity" 
+                              type="number"
+                              value={drink_quantity}
+                              onChange={ e => onChangeDrinks(e) }
+                              />
               </div>
               <div className="p-0 col-sm-2 col-2 d-flex justify-content-center">
-                <Button className='btn-success add-button'>
+                <Button className='btn-success add-button' onClick={addDrink}>
                   <i className='fas fa-plus'></i>
                 </Button>
               </div>
@@ -114,15 +212,25 @@ const OrderScreen = () => {
             <div className="row m-0 p-0 col-12 col-sm-12">
               <div className="p-0 input-wrapper col-10 col-sm-10">
                 <Form.Control as="select"
-                              name="category"
+                              name="salad_name"
                               custom 
+                              value={salad_name}
+                              onChange={ e => onChangeSalads(e)}
                               >
                   <option value="">-- Select a salad --</option>
+                  {
+                    products.map((product) => (
+                      (product.category === 'salads') && <option key={product._id} value={product.name}>{product.name}</option>
+                    ))
+                  }
                 </Form.Control>
-                <Form.Control name="name" type="number"/>
+                <Form.Control type="number" 
+                              name="salad_quantity" 
+                              value={salad_quantity}
+                              onChange={ e => onChangeSalads(e)}/>
               </div>
               <div className="p-0 col-sm-2 col-2 d-flex justify-content-center">
-                <Button className='btn-success add-button'>
+                <Button className='btn-success add-button' onClick={addSalad}>
                   <i className='fas fa-plus'></i>
                 </Button>
               </div>
@@ -130,11 +238,15 @@ const OrderScreen = () => {
           </div>
         </div>
         <div className="col-md-6" >
-          <TableCart cart={cart}/>
+          <TableCart cart={cart} num_table={num_table}/>
         </div>
       </div>
     </>
   )
 }
 
-export default OrderScreen
+const mapStateToProps = state => ({
+  product: state.product,
+})
+
+export default connect(mapStateToProps, {getProducts})(OrderScreen);
