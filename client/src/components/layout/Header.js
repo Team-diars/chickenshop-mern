5c304bc411aea640312b56679d7f12a2e3f94e52
Hector Herrera <hectorsum81@hotmail.com>
2021-06-17 23:30:54 -0500

    restrict navbar roles

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Dropdown, Nav, Navbar } from "react-bootstrap";
 import { connect } from "react-redux";
-import {logout} from '../../actions/auth'
-const Header = ({auth: {isAuthenticated, loading},logout}) => {
-  const guestNavbar = (
+import {logout,loadUser} from '../../actions/auth'
+import { Spinner } from 'react-bootstrap';
+const Header = ({loadUser,auth: {user,isAuthenticated, loading},logout}) => {
+  console.log('userInfo > ',user)
+  const GuestNavbar = (
     <>
       <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-      <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+      <Nav.Link href="/auth"><i className='fas fa-user'></i> Sign In</Nav.Link>
     </>
   )
-  const EmployeeNavbar = (
+  
+  const AdminNavbar = (
     <>
       <Nav.Link href="/profile">
         <i className="fas fa-user"></i> Profile
@@ -36,11 +39,31 @@ const Header = ({auth: {isAuthenticated, loading},logout}) => {
       <Nav.Link href="/settings">
         <i className="fas fa-cog"></i> Settings
       </Nav.Link>
-      <Nav.Link href="/" onClick={logout} style={{color:'#DC3545'}}>
+      <Nav.Link href="/auth" onClick={logout} style={{color:'#DC3545'}}>
         <i className="fas fa-sign-out-alt"></i> Logout
       </Nav.Link>
     </>
   );
+  const CashierNavbar = <>
+    <Nav.Link href="/profile">
+      <i className="fas fa-user"></i> Profile
+    </Nav.Link>
+    <Dropdown>
+      <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+        <i className="fas fa-receipt"></i> Sale
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item href="/orders">Register Order</Dropdown.Item>
+        <Dropdown.Item href="/sales">Generate Sale</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <Nav.Link href="/auth" onClick={logout} style={{color:'#DC3545'}}>
+      <i className="fas fa-sign-out-alt"></i> Logout
+    </Nav.Link>
+  </>
+  useEffect(() => {
+    loadUser();
+  },[loadUser])
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' className="navbar-dark bg-primary" collapseOnSelect>
@@ -49,10 +72,10 @@ const Header = ({auth: {isAuthenticated, loading},logout}) => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-            {(!loading) && (
-              isAuthenticated ? 
-                EmployeeNavbar : guestNavbar
-            )}
+            {
+              (!loading) &&
+              isAuthenticated && user ? (user.role === 'admin') ? AdminNavbar : CashierNavbar : GuestNavbar
+            }
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -65,4 +88,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps,{logout})(Header);
+export default connect(mapStateToProps,{logout,loadUser})(Header);
